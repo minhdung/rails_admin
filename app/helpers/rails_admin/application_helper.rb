a59a6535af7caff925072d4f46c89952fe0c562e
minhdung <dungtm.hedspi.i3@gmail.com>
2013-10-31 10:14:54 +0700

    update template

--- app/helpers/rails_admin/application_helper.rb
+++ app/helpers/rails_admin/application_helper.rb
@@ -75,9 +75,7 @@ module RailsAdmin
     end
 
     def navigation nodes_stack, nodes, level=0
-      ul = '<ul class="dropdown-menu">'
-      ul2 = '</ul>'
-      ul.join.html_safe
+      '<ul class="dropdown-menu">'.join.html_safe
       nodes.map do |node|
         model_param = node.abstract_model.to_param
         url         = url_for(:action => :index, :controller => 'rails_admin/main', :model_name => model_param)
@@ -89,7 +87,7 @@ module RailsAdmin
         end
         li + navigation(nodes_stack, nodes_stack.select{ |n| n.parent.to_s == node.abstract_model.model_name}, level+1)
       end.join.html_safe
-      ul2.join.html_safe
+      '</ul>'.join.html_safe
     end
 
     def breadcrumb action = @action, acc = []
